43a8f41d8a6b6dd91a4b3fee0ab2b2aa23b96a61
Kai Casterline <82246975+SkiGuyKai@users.noreply.github.com>
2021-09-21 12:20:09 -0600

    Add files via upload

---
+++ sector_analysis.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+from alpha_vantage.sectorperformance import SectorPerformances
+import pandas as pd 
+from sector_finder import Translator 
+
+
+# Important Attributes and Methods:
+#     _RAW_SECTORS is the df containing all sector data
+#     rank(number) creates df w/top sectors based on strength
+#         top_sectors is the df
+#     plot_sectors(df) plots bar chart showing sector performances
+#     asp is the df containing only sectors within the portfolio
+
+
+class SectorAnalysis:
+    '''
+    this class will handle the calculations of relevant sectors,
+    i'm still deciding on if i will do bottom-up or top-down portfolio
+    '''
+
+    def __init__(self, portfolio):
+        '''init atts'''
+        self.portfolio = portfolio
+        self._RAW_SECTORS = self.__SECTOR_PERFORMANCE()
+        self.__RS()
+        self.asp = self.__ASSET_SECTOR_PERFORMANCE()
+
+    def __SECTOR_PERFORMANCE(self):
+        '''track the sector performance'''
+        sp = SectorPerformances(key='R4NLQ9F769D3AH9W', output_format='pandas')
+        data, meta_data = sp.get_sector()
+        _RAW_SECTORS = data[['Rank B: Day Performance', 'Rank C: Day Performance', 'Rank D: Month Performance']]
+
+        return _RAW_SECTORS
+
+
+    def __RS(self):
+            '''calculate the RS of each sector using the following equation:
+            RS = 0.6(Rank D)+0.25(Rank C)+0.15(Rank B)'''
+            temp = self._RAW_SECTORS.mul([0.15, 0.25, 0.6] , axis='columns')
+            self._RAW_SECTORS.loc[:, 'Strength'] = temp.sum(axis=1)
+
+
+    def __ASSET_SECTOR_PERFORMANCE(self):
+        '''check sector performance for each asset'''
+        # translate portfolio sectors first!
+        translated_sectors = Translator(self.portfolio)
+        portfolio_sectors = translated_sectors.new_sectors
+
+        # selecting rows based on condition
+        asp = self._RAW_SECTORS.loc[portfolio_sectors]
+
+        return asp
+
+
+    def plot_sectors(self, data):
+        '''plot the sector performance'''
+        data.plot(kind='bar', sharey=True, rot=45,
+            title='Sector Performance', grid=True)
+        plt.show()
+
+
+    def rank(self, number):
+        '''rank the sectors in order of relative strength'''
+        self.top_sectors = self._RAW_SECTORS.nlargest(number, 'Strength')
